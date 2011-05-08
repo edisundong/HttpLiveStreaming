e89fab7c569c0647bfa292fd85db9b3b4b825f99
edisundong <edisundong@gmail.com>
2011-05-08 14:23:45 +0800

    SETUP1423

---
+++ HttpLiveStream/HttpLiveStream/HttpLiveStreamAppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  HttpLiveStreamAppDelegate.h
+//  HttpLiveStream
+//
+//  Created by  apple on 11-5-8.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HttpLiveStreamAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
